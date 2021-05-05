bff7339b3e5af80bc7bf215ecaa6a9b0f0712386
Kruglikov Roman <kruglikov1911@mail.ru>
2021-05-06 00:35:27 +0300

    Branch cleared

--- functional_homework/fasta_translater.py
+++
@@ -1,9 +0,0 @@
-from Bio import SeqIO
-
-
-def translated_fasta_reader(path, codon_table="Standard"):
-    yield from map(lambda x: x.seq.translate(codon_table), SeqIO.parse(path, "fasta"))
-
-
-if __name__ == "__main__":
-    print(list(translated_fasta_reader("example_fasta.fna")))
